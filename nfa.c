873957545ea783ad89926bcc6834aac77b74d9e1
trickyloki3 <tricky.loki3@gmail.com>
2021-10-24 16:10:40 -0700

    * Implemented token_parse's tokenization algorithm.

--- nfa.c
+++ nfa.c
@@ -5,6 +5,8 @@ static int span_push(struct lex *, int, int, int, int);
 static void node_cat(struct lex *, int, int);
 static void node_sub(struct lex *, int, int);
 
+static int token_parse(struct lex *, char *);
+
 static int node_push(struct lex * lex, int * node) {
     int i;
 
@@ -88,4 +90,96 @@ static void node_sub(struct lex * lex, int x, int y) {
     for(i = lex->span_part; i < lex->span_next; i++)
         if(lex->span[i].node == x)
             lex->span[i].node = y;
+}
+
+static int token_parse(struct lex * lex, char * s) {
+    int a;
+    int b;
+
+loop:
+    switch(*s) {
+        case '(':
+            s++;
+            goto loop;
+        case ')':
+            s++;
+            goto loop;
+        case '*':
+            s++;
+            goto loop;
+        case '+':
+            s++;
+            goto loop;
+        case '?':
+            s++;
+            goto loop;
+        case '|':
+            s++;
+            goto loop;
+        case '[':
+
+    span:
+            a = 0;
+            b = 0;
+
+    a:
+            s++;
+            switch(*s) {
+                case '0': a = (a * 16) + 0x0; goto a;
+                case '1': a = (a * 16) + 0x1; goto a;
+                case '2': a = (a * 16) + 0x2; goto a;
+                case '3': a = (a * 16) + 0x3; goto a;
+                case '4': a = (a * 16) + 0x4; goto a;
+                case '5': a = (a * 16) + 0x5; goto a;
+                case '6': a = (a * 16) + 0x6; goto a;
+                case '7': a = (a * 16) + 0x7; goto a;
+                case '8': a = (a * 16) + 0x8; goto a;
+                case '9': a = (a * 16) + 0x9; goto a;
+                case 'A': a = (a * 16) + 0xA; goto a;
+                case 'B': a = (a * 16) + 0xB; goto a;
+                case 'C': a = (a * 16) + 0xC; goto a;
+                case 'D': a = (a * 16) + 0xD; goto a;
+                case 'E': a = (a * 16) + 0xE; goto a;
+                case 'F': a = (a * 16) + 0xF; goto a;
+            }
+
+            if(*s == '-') {
+    b:
+                s++;
+                switch(*s) {
+                    case '0': b = (b * 16) + 0x0; goto b;
+                    case '1': b = (b * 16) + 0x1; goto b;
+                    case '2': b = (b * 16) + 0x2; goto b;
+                    case '3': b = (b * 16) + 0x3; goto b;
+                    case '4': b = (b * 16) + 0x4; goto b;
+                    case '5': b = (b * 16) + 0x5; goto b;
+                    case '6': b = (b * 16) + 0x6; goto b;
+                    case '7': b = (b * 16) + 0x7; goto b;
+                    case '8': b = (b * 16) + 0x8; goto b;
+                    case '9': b = (b * 16) + 0x9; goto b;
+                    case 'A': b = (b * 16) + 0xA; goto b;
+                    case 'B': b = (b * 16) + 0xB; goto b;
+                    case 'C': b = (b * 16) + 0xC; goto b;
+                    case 'D': b = (b * 16) + 0xD; goto b;
+                    case 'E': b = (b * 16) + 0xE; goto b;
+                    case 'F': b = (b * 16) + 0xF; goto b;
+                }
+            } else {
+                b = a;
+            }
+
+            if(*s == ',')
+                goto span;
+
+            if(*s != ']')
+                return panic("invalid character");
+
+            s++;
+            goto loop;
+    }
+
+    if(*s != 0)
+        return panic("invalid character");
+
+    return 0;
 }
